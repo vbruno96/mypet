f141f2bf25c49651a63b36b4464a3e9c3a16290f
Rafael Dias Campos <66502547+RafaelDiasCampos@users.noreply.github.com>
2020-11-23 22:03:00 -0300

    Adicionado novo layout da tela de login. Adicionada tela de perfil de usuario.

---
+++ src/main/resources/static/js/imgLoad.js
@@ -0,0 +1,11 @@
+const reader = new FileReader();
+const fileInput = document.querySelector("#fotoPerfil");
+const img = document.querySelector(".img-perfil");
+
+reader.onload = e => {
+  img.src = e.target.result;
+}
+fileInput.addEventListener('change', e => {
+  const f = e.target.files[0];
+  reader.readAsDataURL(f);
+})
--- src/main/resources/static/js/perfil.js
+++ src/main/resources/static/js/perfil.js
@@ -1,16 +1,65 @@
 var loggedUser = null;
 
+function logoutUser() {
+  localStorage.removeItem("jwtKey");
+  window.location.replace("/index.html");
+}
+
 function updateUserInfo() {
-    document.getElementById("nomeField").value = loggedUser.nome;
-    document.getElementById("emailField").value = loggedUser.login.email;
-    document.getElementById("descricaoField").value = loggedUser.descricao;
-    document.getElementById("telefoneField").value = loggedUser.telefone;
-    document.getElementById("celularField").value = loggedUser.celular;
-    document.getElementById("cepField").value = loggedUser.cep;
-    document.getElementById("cidadeField").value = loggedUser.cidade;
-    document.getElementById("bairroField").value = loggedUser.bairro;
-    document.getElementById("logradouroField").value = loggedUser.logradouro;
-    document.getElementById("numeroField").value = loggedUser.num_residencia;
+  document.getElementById("nome-display").innerHTML = loggedUser.nome;
+  document.getElementById("nome").value = loggedUser.nome;
+  document.getElementById("email").value = loggedUser.login.email;
+  document.getElementById("descricao").value = loggedUser.descricao;
+  document.getElementById("telefone").value = loggedUser.telefone;
+  document.getElementById("celular").value = loggedUser.celular;
+  document.getElementById("cep").value = loggedUser.cep;
+  document.getElementById("cidade").value = loggedUser.cidade;
+  document.getElementById("bairro").value = loggedUser.bairro;
+  document.getElementById("logradouro").value = loggedUser.logradouro;
+  document.getElementById("imagem").src = "/userImages/" + loggedUser.link_imagem;
+}
+
+function saveUser(e) {
+  e.preventDefault();
+
+  newUser = loggedUser;
+  newUser.nome = document.getElementById("nome").value;
+  loggedUser.login.email = document.getElementById("email").value;
+  loggedUser.descricao = document.getElementById("descricao").value;
+  loggedUser.telefone = document.getElementById("telefone").value;
+  loggedUser.celular = document.getElementById("celular").value;
+  loggedUser.cep = document.getElementById("cep").value;
+  loggedUser.cidade = document.getElementById("cidade").value;
+  loggedUser.bairro = document.getElementById("bairro").value;
+  loggedUser.logradouro = document.getElementById("logradouro").value;
+
+  sendNewUserInfo(newUser);
+}
+
+function sendNewUserInfo(user) {
+  const jwtKey = localStorage.getItem("jwtKey");
+  
+  if (!jwtKey) {
+      window.location.replace("/index.html");
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("PUT", "/usuarios", true);
+  xhr.setRequestHeader('Authorization', jwtKey);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify(user));
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState != 4) return;
+
+    if (this.status == 200) {
+      // getLoggedUser();
+    }
+    else if (this.status == 403) {
+      // window.location.replace("/index.html");
+    }
+
+  };
 }
 
 function getLoggedUser() {
@@ -41,3 +90,5 @@ function getLoggedUser() {
   }
 
 window.onload = getLoggedUser;
+
+document.getElementById("formUserData").onsubmit = saveUser;
